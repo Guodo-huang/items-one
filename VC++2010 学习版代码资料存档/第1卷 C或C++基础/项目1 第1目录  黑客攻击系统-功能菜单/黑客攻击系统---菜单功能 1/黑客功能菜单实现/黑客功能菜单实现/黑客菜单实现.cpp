624b7ccfa0b25fb69e39e934a731c17936da7000
Guodo-huang <561182229@qq.com>
2020-06-14 18:23:02 +0800

    黑客攻击菜单基础版.

---
+++ VC++2010 学习版代码资料存档/第1卷 C或C++基础/项目1 第1目录  黑客攻击系统-功能菜单/黑客攻击系统---菜单功能 1/黑客功能菜单实现/黑客功能菜单实现/黑客菜单实现.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<Windows.h>
+
+/*
+*1.ÍøÕ¾404¹¥»÷
+*2.ÍøÕ¾´Û¸Ä¹¥»÷
+*3.ÍøÕ¾¹¥»÷ĞŞ¸´
+*4.¹¥»÷¼ÇÂ¼ĞŞ¸´
+*5.ÍË³ö
+*/
+
+int main(void){
+	std::cout<<"1.ÍøÕ¾404¹¥»÷"<<std::endl;
+	std::cout<<"2.ÍøÕ¾´Û¸Ä¹¥»÷"<<std::endl;
+	std::cout<<"3.ÍøÕ¾¹¥»÷ĞŞ¸´"<<std::endl;
+	std::cout<<"4.¹¥»÷¼ÇÂ¼²éÑ¯"<<std::endl;
+	std::cout<<"5.ÍË³ö"<<std::endl;
+
+	system("pause");
+	return 0;
+}
