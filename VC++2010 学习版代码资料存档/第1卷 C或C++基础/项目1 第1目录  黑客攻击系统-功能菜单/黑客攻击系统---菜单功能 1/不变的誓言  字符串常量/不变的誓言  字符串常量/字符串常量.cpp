21bebefb0db8b237525cd3e7afa5af2d409be343
Guodo-huang <561182229@qq.com>
2020-06-14 18:24:19 +0800

    不变的誓言文件

---
+++ VC++2010 学习版代码资料存档/第1卷 C或C++基础/项目1 第1目录  黑客攻击系统-功能菜单/黑客攻击系统---菜单功能 1/不变的誓言  字符串常量/不变的誓言  字符串常量/字符串常量.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<Windows.h>
+#include<string>
+
+int main(void){
+	//“我的战斗力是"字符串常量 
+	//10000是整数常量
+	std::cout<<"我的战斗力是"<<10000<<std::endl;
+	//"9527"是字符串常量
+	//38是整数常量
+	std::cout<<"9527"<<"的年龄是"<<38<<std::endl;
+	
+
+	system("pause");
+	return 0;
+}
